d9d9820e3f3b0fa8581d6669f91d3d2935d7c57a
Philip Dai <smartielion@gmail.com>
2015-09-19 13:15:42 -0500

    Using webpack to componentify scss files

--- components/Action.js
+++ components/Action.js
@@ -1,10 +1,11 @@
 var React = require("react");
+var SearchButton = require('./SearchButton.js');
 
 var Action = React.createClass({
 	  render: function() {
 	    return (
 	      <div className="action clearfix">
-	        <a href="#" className="action-button button">This guy has few photos, but a lot of skills.</a>
+					<SearchButton />
 	      </div>
 	    );
 	  }
---
+++ components/SearchButton.js
@@ -0,0 +1,13 @@
+var React = require("react");
+
+var SearchButton = React.createClass({
+  render: function() {
+    return (
+      <a href="#" className="search-button">This guy has few photos, but a lot of skills.</a>
+    );
+  }
+});
+
+module.exports = SearchButton;
+
+

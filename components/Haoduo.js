aebb65ab005df8acc9a561a2864057b020f8c885
Philip Dai <smartielion@gmail.com>
2015-09-18 23:33:47 -0500

    Modify '

--- components/Haoduo.js
+++ components/Haoduo.js
@@ -2,7 +2,7 @@ var NewComponent = React.createClass({
   render: function() {
     return (
       <div className="haoduo clearfix">
-        <h2>Hello world, I'm Philip Dai!</h2>
+        <h2>Hello world, I am Philip Dai!</h2>
         <p>Glad to share stories with you guys here.</p>
         <a href="https://github.com/philipdai" className="visit-my-github">Visit My Github</a>
       </div>

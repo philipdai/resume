4c5600928a9fc860aa7b26e6169aafcc71c21f61
Philip Dai <smartielion@gmail.com>
2015-09-18 23:56:38 -0500

    Including Books.js and Haoduo.js files into index.html

--- components/Action.js
+++ components/Action.js
@@ -1,7 +1,6 @@
 var Action = React.createClass({
 	  render: function() {
 	    return (
-
 	      <div className="action clearfix">
 	        <a href="#" className="action-button button">This guy has few photos, but a lot of skills.</a>
 	      </div>

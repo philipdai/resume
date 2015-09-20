1e70c59db6b6f5edfbbd8bba6feace3508782be4
Philip Dai <smartielion@gmail.com>
2015-09-20 02:16:42 -0500

    Refactoring button components

--- components/Action.js
+++ components/Action.js
@@ -1,11 +1,11 @@
-var React = require("react");
+var React = require("react/addons");
 var Button = require('./Button.js');
 
 var Action = React.createClass({
 	  render: function() {
 	    return (
 	      <div className="action clearfix">
-					<Button text={"This guys has few photos, but many skills."}/>
+					<Button text={"This guys has few photos, but many skills."} isOrange={true}/>
 	      </div>
 	    );
 	  }
--- components/Button.js
+++ components/Button.js
@@ -2,8 +2,14 @@ var React = require("react");
 
 var Button = React.createClass({
   render: function() {
+    var cx = React.addons.classSet;
+    var classes = cx({
+      button: true,
+      blue: this.props.isBlue,
+      orange: this.props.isOrange
+    });
     return (
-      <a href="#" className="button">{this.props.text}</a>
+      <a href="#" className={classes}>{this.props.text}</a>
     );
   }
 });

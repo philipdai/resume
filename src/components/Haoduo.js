423e10b9b6678c9ed36033b7ff33280ba485952b
Philip Dai <smartielion@gmail.com>
2015-09-21 03:22:10 -0500

    Fix bug of the shaking button on iphone6+

--- src/components/Haoduo.js
+++ src/components/Haoduo.js
@@ -4,7 +4,7 @@ var Haoduo = React.createClass({
   render: function() {
     return (
       <div className="haoduo clearfix">
-        <h2>Hello world, I am Philip Dai!</h2>
+        <h2>Hello world. I'm Philip Dai!</h2>
         <p>Glad to share stories with you guys here.</p>
         <a href="https://github.com/philipdai" className="visit-my-github">Visit My Github</a>
       </div>

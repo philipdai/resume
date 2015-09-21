213ac74a06bf8580d3dfa858a23906ee6b685dc2
Philip Dai <smartielion@gmail.com>
2015-09-20 19:17:05 -0500

    Adding shaking text

--- components/Action.js
+++ components/Action.js
@@ -4,7 +4,7 @@ var Button = require('./Button.js');
 var Action = React.createClass({
 	  render: function() {
 	    return (
-	      <div className="action clearfix">
+	      <div className="action shake shake-constant clearfix">
 					<Button text={"This guys has few photos, but many skills."} isOrange={true}/>
 	      </div>
 	    );
